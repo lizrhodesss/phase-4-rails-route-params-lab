9c482160b856ce61960eb001cc15730d7c8fa7e9
liz <rhodes.liz@gmail.com>
2022-09-20 09:49:58 -0400

    commit tree 443406e0ea25b9320a9cc278fa6ebbb8790f75b9

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,11 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    students = Student.all(params[:id])
+    if student.name == params[:name]
+    else
+      students = Student.all
+      render json: students
   end
 
 end
